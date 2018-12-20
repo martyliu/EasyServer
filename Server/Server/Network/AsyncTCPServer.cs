da1169f37794dc0e6e5d24978f3891b0ab7fb518
66振 <943294130@qq.com>
2018-12-20 11:33:14 +0800

    first submit

---
+++ Server/Server/Network/AsyncTCPServer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server.Network
+{
+    public class AsyncTCPServer : IDisposable
+    {
+        private TcpListener _listener;
+        private List<Object> _clients;
+
+        public IPAddress IpAddress { get; private set; }
+        public int Port { get; private set; }
+
+        bool disposed = false;
+
+        public bool IsRunning { get; private set; } = false;
+
+        public AsyncTCPServer(int listenPort) 
+        {
+
+        }
+
+        public AsyncTCPServer(IPEndPoint localEP) : this(localEP.Address, localEP.Port)
+        {
+
+        }
+
+        public AsyncTCPServer(IPAddress localIPAddress, int listenPort)
+        {
+            IpAddress = localIPAddress;
+            Port = listenPort;
+
+            
+            _clients = new List<object>();
+            _listener = new TcpListener(IpAddress, Port);
+            //_listener.AllowNatTraversal(true);
+        }
+
+        public void Start()
+        {
+            
+        }
+
+        
+
+        public void Dispose()
+        {
+            
+        }
+    }
+}
---
+++ Server/Server/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace SimpleServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            IPEndPoint ip = new IPEndPoint(IPAddress.Any, 6666);
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
